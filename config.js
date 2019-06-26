f373649a1e0ef351424347a13a95891374a0288f
Piotr Nowinski <pitred007git@gmail.com>
2019-06-26 23:04:46 +0200

    Almost finish

---
+++ config.js
@@ -0,0 +1,5 @@
+module.export = {
+   GOOGLE_CLIENT_ID: '112691876365-bj7mn3jt7vdd3fl1vr4cj5eoshdlsba3.apps.googleusercontent.com',
+   GOOGLE_CLIENT_SECRET: 'zOc_h1ldNzhEm3uUAiXOxEdE',
+   CALLBACK_URL: 'http://localhost:3000/auth/google/callback'
+};
